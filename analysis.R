9175eb0d2763a84e5d79472edbd8ee132438f022
rowanharrisstoertz <rowanharrisstoertz@gmail.com>
2026-09-22 10:00:12 -0700

    Added analysis.R

---
+++ analysis.R
@@ -0,0 +1,8 @@
+# view the dirst line of mtcars dataset
+head(mtcars)
+
+#calculate the average mpg among all cars, save it as a variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print the mean mpg
+mean_mpg
